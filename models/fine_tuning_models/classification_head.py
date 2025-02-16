b54a0b312e4929d90b28e3901a9f4ff125dfb401
Andre Barbosa <abarbosa0494@gmail.com>
2025-02-16 18:12:18 -0300

    update phi model

--- models/fine_tuning_models/classification_head.py
+++ models/fine_tuning_models/classification_head.py
@@ -3,23 +3,10 @@ from transformers import AutoModelForSequenceClassification
 from peft import LoraConfig, TaskType, get_peft_model
 
 
-ID2LABEL = {
-    0: 0, 
-    1: 40,
-    2: 80,
-    3: 120,
-    4: 160,
-    5: 200
-}
+ID2LABEL = {0: 0, 1: 40, 2: 80, 3: 120, 4: 160, 5: 200}
 
-LABEL2ID = {
-    0: 0, 
-    40: 1,
-    80: 2,
-    120: 3,
-    160: 4,
-    200: 5
-}
+LABEL2ID = {0: 0, 40: 1, 80: 2, 120: 3, 160: 4, 200: 5}
+
 
 def load_model_with_classification_head(cfg: DictConfig):
     model_cfg = cfg.experiments.model
@@ -28,18 +15,19 @@ def load_model_with_classification_head(cfg: DictConfig):
         num_labels=model_cfg.num_labels,
         id2label=ID2LABEL,
         label2id=LABEL2ID,
-        cache_dir=cfg.cache_dir
+        cache_dir=cfg.cache_dir,
     )
     return model
+
 
 def load_classification_head_with_lora(cfg: DictConfig):
     model_cfg = cfg.experiments.model
     lora_config = LoraConfig(
-        r=256,
-        lora_alpha=32,
-        lora_dropout=0.15,
+        r=model_cfg.lora_r,
+        lora_alpha=model_cfg.lora_alpha,
+        lora_dropout=model_cfg.lora_dropout,
         task_type=TaskType.SEQ_CLS,
-        target_modules="all-linear",
+        target_modules=model_cfg.lora_target_modules,
     )
 
     model = AutoModelForSequenceClassification.from_pretrained(
@@ -48,13 +36,12 @@ def load_classification_head_with_lora(cfg: DictConfig):
         id2label=ID2LABEL,
         label2id=LABEL2ID,
         cache_dir=cfg.cache_dir,
-        device_map="cuda", 
-        torch_dtype="auto", 
-        trust_remote_code=True, 
-        attn_implementation="flash_attention_2"
+        device_map="cuda",
+        torch_dtype="auto",
+        trust_remote_code=True,
+        attn_implementation="flash_attention_2",
     )
 
     model = get_peft_model(model, lora_config)
     model.config.pad_token_id = model.config.eos_token_id
     return model
-
