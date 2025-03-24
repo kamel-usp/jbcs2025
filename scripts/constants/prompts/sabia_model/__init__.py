603918e3be85f83f67b4cf45537987e4e240fac6
SilveiraIgor <silveira_igor@hotmail.com>
2025-03-24 16:37:29 -0300

    colocando o _grader no init sabia_model

--- scripts/constants/prompts/sabia_model/__init__.py
+++ scripts/constants/prompts/sabia_model/__init__.py
@@ -1,8 +1,8 @@
-from scripts.constants.prompts.sabia_model.concept1_instructions import CONCEPT1_SYSTEM
-from scripts.constants.prompts.sabia_model.concept2_instructions import CONCEPT2_SYSTEM
-from scripts.constants.prompts.sabia_model.concept3_instructions import CONCEPT3_SYSTEM
-from scripts.constants.prompts.sabia_model.concept4_instructions import CONCEPT4_SYSTEM
-from scripts.constants.prompts.sabia_model.concept5_instructions import CONCEPT5_SYSTEM
+from scripts.constants.prompts.sabia_model.concept1_instructions_grader import CONCEPT1_SYSTEM
+from scripts.constants.prompts.sabia_model.concept2_instructions_grader import CONCEPT2_SYSTEM
+from scripts.constants.prompts.sabia_model.concept3_instructions_grader import CONCEPT3_SYSTEM
+from scripts.constants.prompts.sabia_model.concept4_instructions_grader import CONCEPT4_SYSTEM
+from scripts.constants.prompts.sabia_model.concept5_instructions_grader import CONCEPT5_SYSTEM
 
 __all__ = [
     "CONCEPT1_SYSTEM",
